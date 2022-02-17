8cda38561c0d1238e3b424ea780b4405dd9e0941
nando <muhammad.rezki.ananda@gmail.com>
2022-02-17 22:29:13 +0700

    Add splashscreen

---
+++ app/src/main/java/com/motionlaboratory/e_psyc/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,35 @@
+package com.motionlaboratory.e_psyc.ui.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.motionlaboratory.e_psyc.databinding.ActivitySplashScreenBinding
+import com.motionlaboratory.e_psyc.ui.main.MainActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private val binding: ActivitySplashScreenBinding by lazy {
+        ActivitySplashScreenBinding.inflate(
+            layoutInflater
+        )
+    }
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        delayMain()
+    }
+
+    private fun delayMain() {
+        activityScope.launch {
+            delay(4000)
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
