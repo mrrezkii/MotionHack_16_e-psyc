e626953051a8be5d45dc72299e0b36572041cac9
nando <muhammad.rezki.ananda@gmail.com>
2022-02-17 10:57:25 +0700

    INITIAL COMMIT

---
+++ app/src/main/java/com/motionlaboratory/e_psyc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.motionlaboratory.e_psyc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
